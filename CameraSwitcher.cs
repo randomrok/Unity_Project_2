a63256c4c58d5a4dfe613b879de1e36759cb9621
randomrok <68099802+randomrok@users.noreply.github.com>
2021-04-27 14:39:00 +0800

    Add files via upload
    Used to switch between AutoCam and FreeLookCam on the FreeLookCameraRig object. This is contained in the Unity standard assets Sample scene "AircraftJet2Axis".

---
+++ CameraSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Cameras
+{
+    public class CameraSwitcher : MonoBehaviour
+    {
+        private Cameras.FreeLookCam freeLookCam;
+        private Cameras.AutoCam autoCam;
+
+    // Initialisation of scripts to switch
+    void Start()
+        {
+            freeLookCam = GetComponent<FreeLookCam>();
+            autoCam = GetComponent<AutoCam>();
+        }
+
+        // Check if button is pressed to switch scripts
+        void Update()
+        {
+            if (CrossPlatformInputManager.GetButton("Fire2"))
+            {
+                freeLookCam.enabled = true;
+                autoCam.enabled = false;
+            }else
+            {
+                freeLookCam.enabled = false;
+                autoCam.enabled = true;
+                freeLookCam.ResetCamera();
+            }
+        }
+    }
+}
